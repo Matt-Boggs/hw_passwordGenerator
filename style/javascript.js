22fdad022c43a8195a0af1893820b79571a32f07
Matt Boggs <matt.david.boggs@gmail.com>
2020-09-23 19:52:16 -0600

    I now have a few working functions to call on later

--- style/javascript.js
+++ style/javascript.js
@@ -1,28 +1,85 @@
+// var alphabetHigher = alphabetLower.toUpperCase();
+// console.log(alphabetHigher);
+// toUpperCase function doesn't work on arrays, so for now:
 var alphabetLower = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-var alphabetHigher = alphabetLower.toUpperCase();
-console.log(alphabetHigher);
-// toUpperCase function doesn't work on arrays
+var alphabetHigher = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 var numbers = [0,1,2,3,4,5,6,7,8,9];
 var characters = ["!","@","#","$","%","^","&","*"];
+var passLengthString = 8 //just for now
+
+// choose function var go here
+
+var aLChoice = 0
+var aHChoice = 0
+var numChoice = 0
+var charChoice = 0
+
+// concatenate, it's a wonderful phrase, it means no worries for the rest of your days
+var charArr = [ alphabetLower, alphabetHigher, numbers, characters ]
+
+function aLChoose(){
+    aLChoice = Math.floor(Math.random() * alphabetLower.length);
+    return aLChoice;
+}
+function aHChoose(){
+    aHChoice = Math.floor(Math.random() * alphabetHigher.length);
+    return aHChoice;    
+}
+function numChoose(){
+    numChoice = Math.floor(Math.random() * numbers.length);
+    return numChoice;
+}
+function charChoose(){
+    charChoice = Math.floor(Math.random() * characters.length);
+    return charChoice;
+}
 
 
-function generatePass(){
+function generatePassQuestions(){
     var passLengthString = prompt("How long would you like your password to be? Must be between 8 and 128 characters");
         passLengthString = parseInt(passLengthString,10);
         console.log(passLengthString);
+        passLow = confirm("Use lowercase letters?");
+        passHigh = confirm("Use uppercase letters?");
+        passNum = confirm("Use numeric characters?");
+        passSpec = confirm("Use special characters?")
+
     if (passLengthString < 8 || passLengthString > 128){
             alert("Password must be between 8 and 128 characters")
-            generatePass()
+            generatePassQuestions()
         } else if (passLengthString === NaN){
             alert("You must enter a number between 8 and 128")
-            generatePass()
+            generatePassQuestions()
+        } else if (passLow === false && passHigh === false && passNum === false && passSpec === false){
+            alert("You must choose at least one type of character for your password")
+            generatePassQuestions();
+        } else {
+            generatePassActual();
         }
     
     
 
 }
 
-// generatePass()
+function generatePassActual(){
+    for ( i = 0; i < passLengthString; i++) {
+        
+        // [i[x]]  ??
+
+
+
+    }
+
+}
+// generatePassActual()
+aLChoose()
+console.log(aLChoice)
+aHChoose()
+console.log(aHChoice)
+numChoose()
+console.log(numChoice)
+charChoose()
+console.log(charChoice)
 
 
 
